03d461c93cd298df8629c6a8a96985d4dd23a183
Alan Briolat <alan@sanity.(none)>
2007-07-20 16:07:40 +0100

    Added some variables to various modules, updated the structure outline

---
+++ F/Params_Site_mod.f90
@@ -0,0 +1,5 @@
+        module Params_Site_mod
+
+            real, public    :: Rsoil
+
+        end module Params_Site_mod
--- F/Params_Veg_mod.f90
+++ F/Params_Veg_mod.f90
@@ -2,4 +2,14 @@
 
             real, public, save :: LAI
 
+            ! Growing season parameters
+            real, public    :: T_min = 0    ! oC min temp for g
+            real, public    :: T_opt = 21   ! oC opt temp for g
+            real, public    :: T_max = 35   ! oC max temp for g
+            real, public    :: VPD_min = 3.25   ! VPD for min g
+            real, public    :: VPD_max = 1.0    ! VPD for max g
+            real, public    :: SWP_min = -1.25  ! SWP for min g
+            real, public    :: SWP_max = -0.05  ! SWP for max g
+
+
         end module Params_Veg_mod
