7405f529fef1a4441d0aaf9ea77f7038ffca2d2a
Alan Briolat <alan@iapetus.(none)>
2007-08-03 07:24:23 +0100

    Some more wxwidgets experiments

--- interface/app.py
+++ interface/app.py
@@ -10,9 +10,20 @@ class MyApp(wx.App):
         return True
 
     def init_frame(self):
-        self.frame = self.res.LoadDialog(None, 'DIALOG2')
+        self.frame = self.res.LoadDialog(None, 'FileChooser')
+        self.dirlist = xrc.XRCCTRL(self.frame, 'FileChooser_DirList')
+        self.frame.Bind(wx.EVT_CLOSE, self.OnQuit, id=xrc.XRCID('FileChooser'))
         self.frame.Show()
+
+    def OnQuit(self, event):
+        path = self.dirlist.GetFilePath()
+        if path:
+            print path
+        else:
+            print "No path selected"
+        self.Exit()
 
 if __name__ == '__main__':
     app = MyApp(False)
+    print wx.FileSelector("Open a file")
     app.MainLoop()
