87e727e7cacd6130734619162d980129064ea4de
Alan Briolat <alan@iapetus.(none)>
2007-08-15 22:48:54 +0100

    Python interface edits

--- interface/app.py
+++ interface/app.py
@@ -3,27 +3,11 @@
 import wx
 from wx import xrc
 
-class MyApp(wx.App):
+class DOSEApp(wx.App):
     def OnInit(self):
-        self.res = xrc.XmlResource('DO3SE.xrc')
-        self.init_frame()
         return True
 
-    def init_frame(self):
-        self.frame = self.res.LoadDialog(None, 'FileChooser')
-        self.dirlist = xrc.XRCCTRL(self.frame, 'FileChooser_DirList')
-        self.frame.Bind(wx.EVT_CLOSE, self.OnQuit, id=xrc.XRCID('FileChooser'))
-        self.frame.Show()
-
-    def OnQuit(self, event):
-        path = self.dirlist.GetFilePath()
-        if path:
-            print path
-        else:
-            print "No path selected"
-        self.Exit()
-
 if __name__ == '__main__':
-    app = MyApp(False)
+    app = DOSEApp(False)
     print wx.FileSelector("Open a file")
     app.MainLoop()
