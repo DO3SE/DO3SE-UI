4f0c288a839d7208f541fec2d1a2f67ca041ca16
Alan Briolat <alan@iapetus.(none)>
2007-08-22 09:47:00 +0100

    More app stuff

--- interface/app.py
+++ interface/app.py
@@ -2,21 +2,7 @@
 
 from tools import _verbose
 
-import do3se_xrc as ui
+import application
 
-import os
-import wx
-
-#import w_main
-
-class DOSEApp(wx.App):
-    def OnInit(self):
-        #self.window = w_main.Main(None)
-        self.window = ui.xrcwindow_main(None)
-        self.window.Show()
-        return True
-
-if __name__ == '__main__':
-    app = DOSEApp(False)
-    #print wx.FileSelector("Open a file")
-    app.MainLoop()
+app = application.Application(False)
+app.MainLoop()
--- interface/inputfile.py
+++ interface/inputfile.py
@@ -1,9 +1,41 @@
-#!/usr/bin/python
+import csv, copy, exceptions
 
-import types, csv, copy
+class InputFileError(exceptions.Exception):
+    pass
 
 class InputFile:
-    def __init__(self, file, fields):
+    def __init__(self, path):
+        """Only store the path to the file here.
+        """
+        self.path = path
+        self.fields = None
+        self.data = None
+        
+    def __iter__(self):
+        """Allow 'for row in myinputfile:' constructs.
+        """
+        if not self.data:
+            raise InputFileError("Data not yet loaded")
+        else:
+            return iter(self.data)
+
+    def SetFields(self, fields):
+        """Set the field order for the CSV.
+        """
+        self.fields = fields
+
+    def GetFields(self):
+        """Retrieve the CSV field order.
+        """
+        return self.fields
+
+    def Load(self):
+        """Load the data from the CSV file.
+        """
+
+        if not self.fields:
+            raise InputFileError("Field order not yet set")
+
         file = open(file)
 
         self.linecount = 0
@@ -16,9 +48,6 @@ class InputFile:
             self.data.append({'lineno': self.linecount, 'data': copy.deepcopy(l)})
 
         close(file)
-        
-    def __iter__(self):
-        return iter(self.data)
 
 if __name__ == "__main__":
     f = InputFile('../2003_input.csv', ['mm', 'mdd', 'dd', 'hr', 'ts_c', 'vpd', 'precip', 'uh', 'o3_ppb_zr', 'idrctt', 'idfuse', 'zen'])
