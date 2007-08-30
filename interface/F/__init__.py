2d493226c474b22383f64961cbb184555a2104be
Alan Briolat <alan@iapetus.(none)>
2007-08-31 00:32:24 +0100

    Added functions for setting model parameters

--- interface/F/__init__.py
+++ interface/F/__init__.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python
 
 from do3se import *
+
+def SetSiteParam(param, value):
+    setattr(params_site, param, value)
+
+def SetVegParam(param, value):
+    setattr(params_veg, param, value)
