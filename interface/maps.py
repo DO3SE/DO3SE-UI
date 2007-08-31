e250b721d38ea6caeecfc5e0eda35e74decc46f8
Alan Briolat <alan@iapetus.(none)>
2007-08-31 06:28:45 +0100

    Added short-long field name mappings

---
+++ interface/maps.py
@@ -0,0 +1,17 @@
+input_fields = {
+        'yr'        : 'Year',
+        'mm'        : 'Month',
+        'mdd'       : 'Day of Month',
+        'dd'        : 'Day of Year',
+        'hr'        : 'Hour',
+        'ts_c'      : 'Temperature (Celcius)',
+        'vpd'       : 'Vapour Pressure Deficit (Pa)',
+        'precip'    : 'Precipitation (mm)',
+        'uh'        : 'Wind speed (m/s)',
+        'o3_ppb_zr' : 'O3 density (parts per billion)',
+        'idrctt'    : 'Direct radiation',
+        'idfuse'    : 'Diffuse radiation',
+        'zen'       : 'Zenith angle',
+        }
+
+input_fields_reverse = dict([(v, k) for k, v in input_fields.items()])
