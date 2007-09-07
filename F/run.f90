740a7d51c2b1591ba30c1f403172ecf4bf478d3c
Alan Briolat <alan@iapetus.(none)>
2007-09-07 05:55:03 +0100

    Updated test run module

--- F/run.f90
+++ F/run.f90
@@ -7,16 +7,25 @@ contains
     subroutine init()
         use Soil
         use Variables
+        use Params_Site, only: Derive_Windspeed_d_zo, Copy_Windspeed_h_d_zo, &
+                Derive_O3_d_zo, Copy_O3_h_d_zo
+        use Params_Veg, only: Derive_d_zo
         call Soil_initialize()
         AEt = 0
         PEt = 0
         Ei = 0
         AFstY = 0
         AOT40 = 0
+        
+        call Derive_d_zo()
+        !call Derive_Windspeed_d_zo()
+        call Copy_Windspeed_h_d_zo()
+        !call Derive_O3_d_zo()
+        call Copy_O3_h_d_zo()
     end subroutine init
 
     subroutine Do_Calcs()
-        use Inputs, only: Input_sanitize, dd
+        use Inputs, only: dd, Derive_R, Derive_PAR, Derive_ustar
         use Variables, only: dd_prev
         use Phenology, Calc_SAI => Calc_SAI_Simple
         use R
@@ -27,17 +36,22 @@ contains
         use O3_Flux
         use O3_Effects
         
-        call Input_sanitize()
+        ! Run input derivations
+        !call Derive_R()
+        !call Derive_PAR()
+        call Derive_ustar()
 
         call Calc_LAI()     !
         call Calc_SAI()     !
         call Calc_fphen()   !
 
-        call Calc_flight()  !
+        call Calc_sinB()
+        call Calc_Flight()
+        call Calc_Rn()
+
         call Calc_ftemp()   !
         call Calc_fVPD()    !
     
-        call Calc_ustar()   !
         call Calc_Ra()      !
         call Calc_Rb()      !
         call Calc_Rgs()     !
