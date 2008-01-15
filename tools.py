d8f69f14ecbb1da71f6e14aa7f299b7facd8ac35
Alan Briolat <alan@codescape.net>
2008-01-15 19:20:33 +0000

    Added 'truedict()' which pulls the values usually in obj.__dict__ using getattr() instead - needed for getting data out of a Fortran module Signed-off-by: Alan Briolat <alan@codescape.net>

--- tools.py
+++ tools.py
@@ -5,3 +5,9 @@ VERBOSE = True
 def _verbose(msg):
     if VERBOSE:
         sys.stderr.write(msg + '\n')
+
+def truedict(obj):
+    ret = {}
+    for k in obj.__dict__.keys():
+        ret[k] = getattr(obj, k)
+    return ret
