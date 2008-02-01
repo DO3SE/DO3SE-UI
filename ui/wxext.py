1139e4602492dd2a9d95c7774d25a1613e9af535
Alan Briolat <alan@codescape.com>
2008-02-01 23:40:06 +0000

    Added StaticBox2Col for common input panels

--- ui/wxext.py
+++ ui/wxext.py
@@ -326,3 +326,14 @@ class PresetChooser(wx.Panel):
 
     def post_update(self):
         pass
+
+
+
+class StaticBox2Col(wx.StaticBoxSizer):
+    def __init__(self, parent, label):
+        wx.StaticBoxSizer.__init__(self, 
+                wx.StaticBox(parent, label=label), wx.VERTICAL)
+        self.fgs = wx.FlexGridSizer(cols=2, vgap=6, hgap=6)
+        #self.fgs.AddGrowableCol(0)
+        #self.fgs.AddGrowableCol(1)
+        self.Add(self.fgs, 1, wx.EXPAND|wx.ALL, 6)
