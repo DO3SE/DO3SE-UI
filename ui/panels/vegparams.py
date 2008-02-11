15c2a50cbb025d39f0c4bc90535aa99ce3c02431
Alan Briolat <alan@codescape.net>
2008-02-11 11:02:45 +0000

    Added some more veg params

--- ui/panels/vegparams.py
+++ ui/panels/vegparams.py
@@ -30,37 +30,28 @@ class VegParams(wx.Panel):
         sVegParams.AddGrowableCol(1)
         s.Add(sVegParams, 0, wx.ALL|wx.EXPAND, 6)
 
-        # Growth parameters
-        sGrowth = wxext.StaticBox2Col(self, "Growth parameters")
-        sVegParams.Add(sGrowth, 0, wx.EXPAND)
-        sGrowth.fgs.Add(wx.StaticText(self, label="Minimum temperature (Celcius)"),
+        # Characteristics
+        sChar = wxext.StaticBox2Col(self, "Characteristics")
+        sVegParams.Add(sChar, 0, wx.EXPAND)
+        sChar.fgs.Add(wx.StaticText(self, label="Canopy height (m)"),
                 0, wx.ALIGN_CENTER_VERTICAL)
-        self.fields['t_min'] = wx.SpinCtrl(self, min=-10, max=100, initial=0)
-        sGrowth.fgs.Add(self.fields['t_min'], 0)
-        sGrowth.fgs.Add(wx.StaticText(self, label="Optimum temperature (Celcius)"),
+        self.fields['h'] = wx.SpinCtrl(self, min=1, max=100, initial=25)
+        sChar.fgs.Add(self.fields['h'], 0)
+        sChar.fgs.Add(wx.StaticText(self, label="Root depth (m)"),
                 0, wx.ALIGN_CENTER_VERTICAL)
-        self.fields['t_opt'] = wx.SpinCtrl(self, min=-10, max=100, initial=21)
-        sGrowth.fgs.Add(self.fields['t_opt'])
-        sGrowth.fgs.Add(wx.StaticText(self, label="Maximum temperature (Celcius)"),
+        self.fields['root'] = FloatSpin(self, value=1.2, min_val=0.01, 
+                increment=0.1, digits=1)
+        sChar.fgs.Add(self.fields['root'], 0)
+        sChar.fgs.Add(wx.StaticText(self, label="Min. Leaf Area Index (m^2/m^2)"),
                 0, wx.ALIGN_CENTER_VERTICAL)
-        self.fields['t_max'] = wx.SpinCtrl(self, min=-10, max=100, initial=35)
-        sGrowth.fgs.Add(self.fields['t_max'], 0)
-
-        # Maintain integrity on growth parameters
-        def f(evt):
-            if self.fields['t_opt'].GetValue() < self.fields['t_min'].GetValue():
-                self.fields['t_opt'].SetValue(self.fields['t_min'].GetValue())
-            elif self.fields['t_opt'].GetValue() > self.fields['t_max'].GetValue():
-                self.fields['t_opt'].SetValue(self.fields['t_max'].GetValue())
-        self.Bind(wx.EVT_SPINCTRL, f, self.fields['t_opt'])
-        def f(evt):
-            if self.fields['t_min'].GetValue() > self.fields['t_opt'].GetValue():
-                self.fields['t_min'].SetValue(self.fields['t_opt'].GetValue())
-        self.Bind(wx.EVT_SPINCTRL, f, self.fields['t_min'])
-        def f(evt):
-            if self.fields['t_max'].GetValue() < self.fields['t_opt'].GetValue():
-                self.fields['t_max'].SetValue(self.fields['t_opt'].GetValue())
-        self.Bind(wx.EVT_SPINCTRL, f, self.fields['t_max'])
+        self.fields['lai_min'] = FloatSpin(self, value=0.0, min_val=0,
+                increment=0.1, digits=1)
+        sChar.fgs.Add(self.fields['lai_min'], 0)
+        sChar.fgs.Add(wx.StaticText(self, label="Max. Leaf Area Index (m^2/m^2)"),
+                0, wx.ALIGN_CENTER_VERTICAL)
+        self.fields['lai_max'] = FloatSpin(self, value=4.0, min_val=0,
+                increment=0.1, digits=1)
+        sChar.fgs.Add(self.fields['lai_max'], 0)
 
         # Growing season
         sSeason = wxext.StaticBox2Col(self, "Growing season")
@@ -100,11 +91,69 @@ class VegParams(wx.Panel):
                 self.fields['aend'].SetValue(self.fields['astart'].GetValue())
         self.Bind(wx.EVT_SPINCTRL, f, self.fields['aend'])
 
+        # Environmental dependence
+        sGrowth = wxext.StaticBox2Col(self, "Environmental dependence")
+        sVegParams.Add(sGrowth, 0, wx.EXPAND)
+        sGrowth.fgs.Add(wx.StaticText(self, label="Minimum temperature (Celcius)"),
+                0, wx.ALIGN_CENTER_VERTICAL)
+        self.fields['t_min'] = wx.SpinCtrl(self, min=-10, max=100, initial=0)
+        sGrowth.fgs.Add(self.fields['t_min'], 0)
+        sGrowth.fgs.Add(wx.StaticText(self, label="Optimum temperature (Celcius)"),
+                0, wx.ALIGN_CENTER_VERTICAL)
+        self.fields['t_opt'] = wx.SpinCtrl(self, min=-10, max=100, initial=21)
+        sGrowth.fgs.Add(self.fields['t_opt'])
+        sGrowth.fgs.Add(wx.StaticText(self, label="Maximum temperature (Celcius)"),
+                0, wx.ALIGN_CENTER_VERTICAL)
+        self.fields['t_max'] = wx.SpinCtrl(self, min=-10, max=100, initial=35)
+        sGrowth.fgs.Add(self.fields['t_max'], 0)
+        # TODO: min/max values for VPD
+        sGrowth.fgs.Add(wx.StaticText(self, label="VPD for minimum growth"),
+                0, wx.ALIGN_CENTER_VERTICAL)
+        self.fields['vpd_min'] = FloatSpin(self, value=3.25, increment=0.01, digits=2)
+        sGrowth.fgs.Add(self.fields['vpd_min'], 0)
+        sGrowth.fgs.Add(wx.StaticText(self, label="VPD for maximum growth"),
+                0, wx.ALIGN_CENTER_VERTICAL)
+        self.fields['vpd_max'] = FloatSpin(self, value=1.0, increment=0.01, digits=2)
+        sGrowth.fgs.Add(self.fields['vpd_max'], 0)
+        # TODO: min/max for SWP
+        sGrowth.fgs.Add(wx.StaticText(self, label="SWP for minimum growth"),
+                0, wx.ALIGN_CENTER_VERTICAL)
+        self.fields['swp_min'] = FloatSpin(self, value=-1.25, increment=0.01, digits=2)
+        sGrowth.fgs.Add(self.fields['swp_min'], 0)
+        sGrowth.fgs.Add(wx.StaticText(self, label="SWP for maximum growth"),
+                0, wx.ALIGN_CENTER_VERTICAL)
+        self.fields['swp_max'] = FloatSpin(self, value=-0.5, increment=0.01, digits=2)
+        sGrowth.fgs.Add(self.fields['swp_max'], 0)
+
+        # Maintain integrity on environmental dependence
+        def f(evt):
+            if self.fields['t_opt'].GetValue() < self.fields['t_min'].GetValue():
+                self.fields['t_opt'].SetValue(self.fields['t_min'].GetValue())
+            elif self.fields['t_opt'].GetValue() > self.fields['t_max'].GetValue():
+                self.fields['t_opt'].SetValue(self.fields['t_max'].GetValue())
+        self.Bind(wx.EVT_SPINCTRL, f, self.fields['t_opt'])
+        def f(evt):
+            if self.fields['t_min'].GetValue() > self.fields['t_opt'].GetValue():
+                self.fields['t_min'].SetValue(self.fields['t_opt'].GetValue())
+        self.Bind(wx.EVT_SPINCTRL, f, self.fields['t_min'])
+        def f(evt):
+            if self.fields['t_max'].GetValue() < self.fields['t_opt'].GetValue():
+                self.fields['t_max'].SetValue(self.fields['t_opt'].GetValue())
+        self.Bind(wx.EVT_SPINCTRL, f, self.fields['t_max'])
+
     def getvalues(self):
-        return {
+        return { 
+            'h':        float(self.fields['h'].GetValue()),
+            'root':     float(self.fields['root'].GetValue()),
+            'lai_min':  float(self.fields['lai_min'].GetValue()),
+            'lai_max':  float(self.fields['lai_max'].GetValue()),
             't_min':    float(self.fields['t_min'].GetValue()),
             't_opt':    float(self.fields['t_opt'].GetValue()),
             't_max':    float(self.fields['t_max'].GetValue()),
+            'vpd_min':  float(self.fields['vpd_min'].GetValue()),
+            'vpd_max':  float(self.fields['vpd_max'].GetValue()),
+            'swp_min':  float(self.fields['swp_min'].GetValue()),
+            'swp_max':  float(self.fields['swp_max'].GetValue()),
             'sgs':      float(self.fields['sgs'].GetValue()),
             'egs':      float(self.fields['egs'].GetValue()),
             'astart':   float(self.fields['astart'].GetValue()),
@@ -112,9 +161,17 @@ class VegParams(wx.Panel):
         }
 
     def setvalues(self, v):
+        self.fields['h'].SetValue(v['h'])
+        self.fields['root'].SetValue(v['root'])
+        self.fields['lai_min'].SetValue(v['lai_min'])
+        self.fields['lai_max'].SetValue(v['lai_max'])
         self.fields['t_min'].SetValue(v['t_min'])
         self.fields['t_opt'].SetValue(v['t_opt'])
         self.fields['t_max'].SetValue(v['t_max'])
+        self.fields['vpd_min'].SetValue(v['vpd_min'])
+        self.fields['vpd_max'].SetValue(v['vpd_max'])
+        self.fields['swp_min'].SetValue(v['swp_min'])
+        self.fields['swp_max'].SetValue(v['swp_max'])
         self.fields['sgs'].SetValue(v['sgs'])
         self.fields['egs'].SetValue(v['egs'])
         self.fields['astart'].SetValue(v['astart'])
