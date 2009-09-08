ee6228dd4f982488246d44c35431d0eafbca6761
Alan Briolat <sei@alanbriolat.co.uk>
2009-09-08 16:15:54 +0100

    Added basic config sanitisation

--- ui/config.py
+++ ui/config.py
@@ -27,4 +27,22 @@ from jsondict import JsonDict
 from copy import deepcopy
 
 def open_config(filename):
+    """
+    Open a JSON file as a dict
+    """
     return JsonDict(filename, deepcopy(defaults))
+
+import dose
+
+def sanitise_config(config):
+    """
+    Sanitise a configuration dict
+
+    Perform little cleanups like removing references to fields that don't exist
+    """
+    # Remove references to non-existant input fields
+    for key, preset in config['input_fields'].iteritems():
+        config['input_fields'][key] = [x for x in preset if x in input_field_map]
+    # Remove references to non-existant output fields
+    for key, preset in config['output_fields'].iteritems():
+        config['output_fields'][key] = [x for x in preset if x in output_field_map]
