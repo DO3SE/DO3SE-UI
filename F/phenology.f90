65b5dabcc1856adc2948ca7a87a175aa1a0957c0
Alan Briolat <sei@alanbriolat.co.uk>
2009-09-23 10:08:47 +0100

    Replaced phenology params, fphen + LAI calculations

--- F/phenology.f90
+++ F/phenology.f90
@@ -9,16 +9,34 @@ contains
 
     !==========================================================================
     ! LAI calculation based on growing season - uses polygon calculation
+    !
+    !     |        LAI_b                        LAI_c
+    !     |          ______________________________
+    !  L  |         /                              \
+    !  A  |        /                                \
+    !  I  |       /                                  \
+    !     |      /                                    \
+    !     |     /            <1> = LAI_1               \
+    !     |  LAI_a                                   LAI_d
+    !     +--------------------------------------------------
+    !          |<1> |                              | <2>|
+    !         SGS                                      EGS
+    !                       Day of year
     !==========================================================================
     subroutine Calc_LAI()
-        use Functions, only: Polygon
-        use Params_Veg, only: SGS, EGS, Ls, Le, LAI_min, &
-            LAI_max
+        use Params_Veg, only: SGS, EGS, LAI_a, LAI_b, LAI_c, LAI_d, LAI_1, LAI_2
         use Inputs, only: dd
         use Variables, only: LAI
 
-        LAI = Polygon(SGS, SGS + Ls, EGS - Le, EGS, &
-                        LAI_min, LAI_max, LAI_min, dd)
+        if (dd < SGS .or. dd > EGS) then
+            LAI = 0
+        else if (dd < (SGS + LAI_1)) then
+            LAI = LAI_a + (LAI_b - LAI_a) * (dd - SGS) / LAI_1
+        else if (dd < (EGS - LAI_2)) then
+            LAI = LAI_b + (LAI_c - LAI_b) * (dd - (SGS + LAI_1)) / (EGS - SGS - LAI_1 - LAI_2)
+        else if (dd <= EGS) then
+            LAI = LAI_d + (LAI_c - LAI_d) * (EGS - dd) / LAI_2
+        end if
     end subroutine Calc_LAI
 
     !==========================================================================
@@ -33,14 +51,13 @@ contains
     ! SAI calculation for wheat taking growing season into account
     !==========================================================================
     subroutine Calc_SAI_Wheat()
+        use Params_Veg, only: SGS, EGS, LAI_1
         use Inputs, only: dd
-        use Params_Veg, only: SGS, EGS, Ls
-        use Functions, only: Polygon
         use Variables, only: LAI, SAI
 
         if ( dd < SGS .or. dd > EGS ) then
             SAI = LAI
-        else if ( dd < SGS + Ls ) then      ! implicitly > SGS
+        else if ( dd < SGS + LAI_1 ) then      ! implicitly > SGS
             SAI = LAI + ((5/3.5) - 1)*LAI
         else                ! implicitly >= SGS + Ls .and. <= EGS
             SAI = LAI + 1.5
@@ -60,7 +77,7 @@ contains
     ! e    |   |                                             \ fphen_e
     ! n    |   |                                              |
     !      |   |                                              |
-    !  0.0 |___|            <1> = fphen1, etc.                |___________
+    !  0.0 |___|            <1> = fphen_1, etc.               |___________
     !      |
     !      +--------------------------------------------------------------
     !      0   | <1> |       |<2>|         |<3>|       | <4>  |         365
@@ -70,24 +87,28 @@ contains
     !                                Day of Year
     !==========================================================================
     subroutine Calc_fphen()
-        use Functions, only: Polygon
-        use Params_Veg, only: SGS, EGS, Astart, Aend, &
-            fphen_a, fphen_b, fphen_c, fphen_d, fphenS, fphenE
-        use Inputs, only: dd
-        use Variables, only: fphen, leaf_fphen
+        use Params_Veg, only: SGS, EGS
+        use Params_Veg, only: fphen_limA, fphen_limB, fphen_1, fphen_2, fphen_3, fphen_4
+        use Params_Veg, only: fphen_a, fphen_b, fphen_c, fphen_d, fphen_e
+        use Inputs,     only: dd
+        use Variables,  only: fphen, leaf_fphen
 
-        if ( dd <= SGS .or. dd > EGS ) then
-            fphen = 0
-        else if ( dd < Astart ) then
-            fphen = fphen_a
-        else if ( dd < Astart + fphenS ) then
-            fphen = fphen_b + (fphen_c - fphen_b) * (dd - Astart)/fphenS
-        else if ( dd < Aend - fphenE ) then
+        if (dd < SGS .or. dd > EGS) then
+            fphen = 0.0
+        else if (dd < (SGS + fphen_1)) then
+            fphen = fphen_a + (fphen_b + fphen_a) * (dd - SGS) / fphen_1
+        else if (dd < fphen_limA) then
+            fphen = fphen_b
+        else if (dd < (fphen_limA + fphen_2)) then
+            fphen = fphen_b - (fphen_b - fphen_c) * (dd - fphen_limA) / fphen_2
+        else if (dd < (fphen_limB - fphen_3)) then
             fphen = fphen_c
-        else if ( dd < Aend ) then
-            fphen = fphen_d + (fphen_c - fphen_d) * (Aend - dd)/fphenE
-        else
+        else if (dd < fphen_limB) then
+            fphen = fphen_d - (fphen_d - fphen_c) * (fphen_limB - dd) / fphen_3
+        else if (dd < (EGS - fphen_4)) then
             fphen = fphen_d
+        else if (dd <= EGS) then
+            fphen = fphen_e + (fphen_d - fphen_e) * (EGS - dd) / fphen_4
         end if
 
         ! TODO: Change this to calculate fphen for upper canopy leaf?
