76e1dc688ee13cb389db07fe8eaae9950372c86a
Alan Briolat <sei@alanbriolat.co.uk>
2009-09-23 17:07:07 +0100

    Added StaticBoxCanvas - a canvas in a frame

--- ui/wxext/__init__.py
+++ ui/wxext/__init__.py
@@ -6,3 +6,4 @@ from floatctrl import FloatCtrl
 from listselectctrl import ListSelectCtrl
 from presetchooser import PresetChooser
 from staticbox2col import StaticBox2Col
+from staticboxcanvas import StaticBoxCanvas
