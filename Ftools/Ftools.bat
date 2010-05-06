41ad0f2ab6f9352cd082343e03f9205a66168568
Alan Briolat <sei@alanbriolat.co.uk>
2010-05-06 20:13:21 +0100

    Added Ftools scripts and instructions

---
+++ Ftools/Ftools.bat
@@ -0,0 +1,18 @@
+:: Start a Windows shell (cmd.exe) with the environment variables modified to
+:: enable access to MinGW, mSYS and G95
+
+@echo off
+
+set basepath=%~dp0
+set PATH=%basepath%;%PATH%
+set PATH=%basepath%MinGW\bin;%PATH%
+set PATH=%basepath%msys\1.0\bin;%PATH%
+
+set LIBRARY_PATH=%basepath%MinGW\lib;%LIBRARY_PATH%
+
+:: Drop to a normal shell in the normal location
+cd %USERPROFILE%
+cmd
+:: Exit the batch script so the user doesn't get a prompt when exiting the
+:: cmd.exe shell
+exit /B 0
