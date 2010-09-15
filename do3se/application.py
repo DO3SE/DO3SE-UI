409fed6096dafa6a5a8e91bf16e958275dbea27d
Alan Briolat <alan@codescape.net>
2010-09-15 16:55:09 +0100

    Rewrote automation script

--- do3se/application.py
+++ do3se/application.py
@@ -26,24 +26,16 @@ class App(wx.App):
         self.SetAppName(app_name)
         self.title = app_name
 
-        # Figure out some paths...
-        si_filename = '%s-%s.lock' % (self.GetAppName(), wx.GetUserId())
-        config_path = os.path.join(wx.StandardPaths_Get().GetUserDataDir(), 'config.pickle')
-
         # Only allow one instance of the application
+        si_filename = '%s-%s.lock' % (self.GetAppName(), wx.GetUserId())
         self.sichecker = wx.SingleInstanceChecker(si_filename, wx.StandardPaths_Get().GetTempDir())
         if self.sichecker.IsAnotherRunning():
             _log.error("Another instance is already running - exiting!")
             wx.MessageDialog(None, "DO3SE is already running", style=wx.OK|wx.ICON_ERROR).ShowModal()
             return False
 
-        # Make sure configuration directory exists
-        if not os.path.exists(os.path.dirname(config_path)):
-            os.makedirs(os.path.dirname(config_path))
-            _log.info("Created configuration directory: " + os.path.dirname(config_path))
-
         # Load configuration
-        self.config = Config(config_path)
+        self.config = open_config()
         # Load default presets
         self.default_presets = load_presets(open('resources/default_veg_presets.csv', 'r'))
 
@@ -82,6 +74,38 @@ class App(wx.App):
             w.Close()
 
 
+def open_config():
+    """Open configuration file as a :class:`~do3se.config.Config` object.
+    
+    Because this function uses :obj:`wx.StandardPaths`, an application object
+    with a valid application name must be created first.
+    """
+    # Find path where the config file should be
+    filename = os.path.join(wx.StandardPaths_Get().GetUserDataDir(),
+                            'config.pickle')
+    # Create config directory if it doesn't exist
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+        _log.info('Created configuration directory: ' + dirname)
+    # Create and return Config object
+    return Config(filename)
+
+
+def logging_setup(**kwargs):
+    """Setup Python logging.
+
+    *kwargs* are merged with default options and then passed to
+    :func:`logging.basicConfig`.  This function exists so that different
+    methods of running the application can use the same logging setup without
+    duplication.
+    """
+    config = {'format': '[%(levelname)-8s] %(name)s: %(message)s',
+              'level': logging.DEBUG}
+    config.update(kwargs)
+    logging.basicConfig(**config)
+
+
 def main(args):
     from ui import MainWindow, ProjectWindow
 
@@ -98,8 +122,7 @@ def main(args):
     
     (options, args) = parser.parse_args(args)
 
-    logging.basicConfig(format="[%(levelname)-8s] %(name)s: %(message)s",
-                        level=options.loglevel)
+    logging_setup(level=options.loglevel)
 
     # Show a project window for each argument, or launch the main window
     app = App()
---
+++ do3se/automate.py
@@ -0,0 +1,132 @@
+import sys
+import optparse
+import logging
+_log = logging.getLogger('do3se.automate')
+
+import wx
+
+import application
+import model
+from project import Project
+from dataset import Dataset
+
+
+class App(wx.App):
+    """Minimal wxPython application.
+
+    It's not possible to use :obj:`wx.StandardPaths` without first creating an
+    application instance.  This application class uses the same application
+    name as :class:`do3se.application.App` so that paths remain the same.
+    """
+    def OnInit(self):
+        self.SetAppName(application.app_name)
+        self.config = application.open_config()
+        return True
+
+
+def list_outputs(option, opt_str, value, parser, app):
+    """List available output formats and output fields and exit."""
+    print 'Available output format presets'
+    for p in app.config.data['output_formats'].keys():
+        print '\t+' + p
+
+    print 'Available output fields:'
+    for f in model.output_fields.itervalues():
+        print '\t%(variable)-16s %(long)s' % f
+
+    exit(0)
+
+
+def format_option_callback(option, opt_str, value, parser, app):
+    """Parse format option, either as a list of fields or a preset name."""
+    if len(value) == 0:
+        parser.error('Invalid output format')
+
+    if value[0] == '+':
+        preset = value[1:]
+        if preset not in app.config.data['output_formats']:
+            parser.error('Output format preset doesn\'t exist: ' +
+                         preset + ' (see --list-outputs)')
+        parser.values.format = app.config.data['output_formats'][preset]['fields']
+        parser.values.show_headers = app.config.data['output_formats'][preset]['headers']
+        parser.values.reduce_output = app.config.data['output_formats'][preset]['reduce']
+    else:
+        parser.values.format = value.split(',')
+        for f in parser.values.format:
+            if f not in model.output_fields:
+                parser.error('Output field doesn\'t exist: ' + f + ' (see --list-outputs)')
+
+
+def outfile_callback(option, opt_str, value, parser):
+    """Open a different output file."""
+    parser.values.outfile = open(value, 'wb')
+
+
+def main(args):
+    app = App()
+
+    parser = optparse.OptionParser(usage='Usage: %prog [options] projectfile inputfile')
+    parser.add_option('-v', '--verbose',
+                      action='store_const',
+                      dest='loglevel',
+                      const=logging.INFO)
+    parser.add_option('-d', '--debug',
+                      action='store_const',
+                      dest='loglevel',
+                      const=logging.DEBUG)
+    parser.add_option('--list-outputs',
+                      action='callback',
+                      callback=list_outputs,
+                      callback_kwargs={'app': app})
+    parser.add_option('-f', '--format',
+                      action='callback',
+                      callback=format_option_callback,
+                      callback_kwargs={'app': app},
+                      type='string',
+                      nargs=1,
+                      help='A comma-separated list of output field keys or +PRESET '
+                           '(see --list-outputs) [default: all fields]')
+    parser.add_option('-o', '--outfile',
+                      action='callback',
+                      callback=outfile_callback,
+                      type='string',
+                      nargs=1,
+                      help='Write results to OUTFILE [default: use stdout]')
+    parser.add_option('-r', '--reduce',
+                      action='store_const',
+                      dest='reduce_output',
+                      const=True,
+                      help='Only output rows during growing season')
+    parser.add_option('-n', '--no-headers',
+                      action='store_const',
+                      dest='show_headers',
+                      const=False,
+                      help='Don\'t output column headers')
+    parser.set_defaults(loglevel=logging.CRITICAL,
+                        format=model.output_fields.keys(),
+                        show_headers=True,
+                        reduce_output=False,
+                        outfile=sys.stdout)
+    
+    (options, args) = parser.parse_args(args)
+
+    if len(args) < 2:
+        parser.error('Not enough arguments')
+
+    application.logging_setup(level=options.loglevel)
+    projectfile, inputfile = args
+
+    project = Project(projectfile)
+    if not project.exists():
+        parser.error('Project file does not exist: ' + projectfile)
+
+    dataset = Dataset(open(inputfile, 'r'), project.data)
+    results = dataset.run()
+    results.save(options.outfile,
+                 options.format,
+                 options.show_headers,
+                 (project.data['sgs'], project.data['egs']) if options.reduce_output else None)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
