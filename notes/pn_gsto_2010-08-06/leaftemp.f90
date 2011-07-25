d7a8b0e956a2af370531e01233f4d3f9219d0f7c
Alan Briolat <sei@alanbriolat.co.uk>
2011-07-25 16:05:35 +0100

    Documentation and initial code attempt for leaf temperature

---
+++ notes/pn_gsto_2010-08-06/leaftemp.f90
@@ -0,0 +1,47 @@
+module LeafTemp
+
+    implicit none
+
+    real, parameter :: R = 8.3144621    ! Universal gas constant, J mol-1 K-1
+    real, parameter :: M_A = 29.0       ! Molecular weight of air, g mol-1
+    real, parameter :: c_p = 1.010      ! Specific heat capacity of dry air at 
+                                        !   standard pressure and 20 C, J g-1 K-1
+    real, parameter :: K = 273.15       ! Offset between Kelvin and Celcius
+
+contains
+
+    real function Tleaf (Tair, P, Rn, VPD, g_a, g_c)
+        real, intent(in) :: Tair    ! Air temperature, degrees C
+        real, intent(in) :: P       ! Atmospheric pressure, kPa
+        real, intent(in) :: Rn      ! Net radiation, W m-2
+        real, intent(in) :: VPD     ! Vapour pressure deficit, kPa
+        real, intent(in) :: g_a     ! Aerodynamic resistance
+        real, intent(in) :: g_c     ! Canopy resistance
+
+        real :: rho, VDD, e_s, s, lambda, psychro, phi, g_w
+        ! Density of dry air, kg m-3
+        rho = (P * M_A) / (R * (Tair + K))
+        ! Vapour density deficit, kg m-3
+        VDD = rho * 0.622 * VPD / P
+        ! Saturation vapour pressure, kPa
+        e_s = 0.611 * exp(17.27 * Tair / (Tair + 237.3))
+        ! Slope of saturation VDD curve, kg m-3 K-1
+        s = (4098.0 * rho * 0.622 * e_s) / ((Tair + 237.3)**2)
+        ! Latent heat vapourisation of water, J g-1
+        lambda = -0.0000614342*Tair**3 + 0.00158927*Tair**2 - 2.36418*Tair + 2500.79
+        ! Psychrometric parameter, kg m-3 K-1
+        psychro = rho * c_p / lambda
+        ! Energy balance, W m-2
+        phi = Rn
+        ! Combined resistance to water vapour
+        g_w = 1.0 / ((1.0 / g_a) + (1.0 / g_c))
+        ! Leaf temperature, degrees C
+        Tleaf = Tair + ((phi - lambda * g_w * VDD) / (lambda * (s * g_w + psychro * g_a)))
+    end function Tleaf
+
+end module LeafTemp
+
+
+program TestLeafTemp
+
+end program TestLeafTemp
