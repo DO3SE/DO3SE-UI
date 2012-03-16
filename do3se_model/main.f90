a6d794d4e52fb76c89eb33a39e4e80f1daa99c52
Alan Briolat <alan.briolat@gmail.com>
2012-03-16 17:36:23 +0000

    WIP: made a good start on making a fairly robust Fortran program entry point

---
+++ do3se_model/main.f90
@@ -0,0 +1,140 @@
+module Main
+
+    ! Input row format
+    type InputRow
+        integer :: year
+        integer :: month
+        integer :: dayofmonth
+        integer :: dayofyear
+        integer :: hour         ! Hour of day, 0-23
+        real    :: Tair         ! Air temperature, degrees C
+        real    :: Tleaf        ! Leaf temperature, degrees C
+        real    :: VPD          ! Vapour pressure deficit, kPa
+        real    :: u_zu         ! Windspeed at measurement height zu, m s-1
+        real    :: precip       ! Precipitation, mm
+        real    :: P            ! Atmospheric presure, kPa
+        real    :: O3_ppb_zO3   ! O3 concentration at measurement height zO3, ppb
+        real    :: CO2          ! Ambient CO2 concentration, ppm
+        real    :: Hd           ! Sensible heat flux, W m-2
+        real    :: R            ! Global radiation, W m-2
+        real    :: PAR          ! Photosynthetically active radiation, W m-2
+        real    :: Rn_MJ        ! Net radiation, MJ m-2
+        real    :: leaf_fphen   ! Leaf Fphen input, fraction
+    end type InputRow
+
+
+    ! Derived or calculated input data
+    type InputExtra
+!        real    :: PPFD         ! Photosynthetic photon flux density, umol m-2 s-1
+        real    :: Rn_W         ! Net radiation, W m-2
+        logical :: is_beginning_of_day
+    end type InputExtra
+
+
+    ! Input data options, controlling available derivations etc.
+    type InputOptions
+        logical :: calculate_Tleaf
+        logical :: calculate_PAR_from_R
+        logical :: calculate_R_from_PAR
+        logical :: use_constant_CO2
+        real    :: constant_CO2
+        logical :: calculate_Rn
+        logical :: use_input_leaf_fphen
+    end type InputOptions
+
+contains
+
+    subroutine stderr(message)
+        use ISO_FORTRAN_ENV, only: ERROR_UNIT
+        character(len=*), intent(in) :: message
+        WRITE (ERROR_UNIT, '(a)') message
+    end subroutine stderr
+
+end module Main
+
+
+program DO3SE
+
+    use ISO_FORTRAN_ENV, only: INPUT_UNIT, OUTPUT_UNIT, IOSTAT_EOR, IOSTAT_END
+    use Main
+
+    character(len=*), parameter :: usage = "usage: do3se config.nml [inputfile [outputfile]]"
+
+    integer :: inunit = INPUT_UNIT
+    integer :: outunit = OUTPUT_UNIT
+    integer :: configunit
+    integer :: errstat
+    character(len=256) :: argbuffer
+    integer :: arglen
+
+    type(InputOptions) :: input
+    namelist /do3se_config/ input
+
+    character(len=512) :: linebuffer
+    type(InputRow) :: inrow
+
+    ! Load configuration file
+    call GET_COMMAND_ARGUMENT(1, argbuffer, arglen, errstat)
+    if (arglen == 0 .or. errstat > 0) then
+        call stderr("No configuration file specified")
+        call stderr(usage)
+        call EXIT(1)
+    else if (errstat < 0) then
+        call stderr("Configuration file path too long")
+        call EXIT(1)
+    end if
+    OPEN (newunit=configunit, file=argbuffer, status="old", action="read", position="rewind")
+    READ (unit=configunit, nml=do3se_config, iostat=errstat)
+    if (errstat /= 0) then
+        call stderr("Error reading configuration file")
+        call EXIT(1)
+    end if
+    CLOSE (configunit)
+
+    ! Open input file, if specified
+    call GET_COMMAND_ARGUMENT(2, argbuffer, arglen, errstat)
+    if (errstat < 0) then
+        call stderr("Input file path too long")
+        call EXIT(1)
+    else if (arglen > 0 .and. errstat == 0) then
+        OPEN (newunit=inunit, file=argbuffer, status="old", action="read", position="rewind")
+    end if
+
+    ! Open output file, if specified
+    call GET_COMMAND_ARGUMENT(3, argbuffer, arglen, errstat)
+    if (errstat < 0) then
+        call stderr("Output file path too long")
+        call EXIT(1)
+    else if (arglen > 0 .and. errstat == 0) then
+        OPEN (newunit=outunit, file=argbuffer, status="replace", action="write", position="rewind")
+    end if
+
+
+    do
+        ! Force line-at-a-time reads
+        READ (unit=inunit, fmt='(a)', iostat=errstat) linebuffer
+        if (errstat == IOSTAT_END) then
+            call stderr("Reached end of input file")
+            exit
+        end if
+
+        ! Read row data from the input
+        READ (unit=linebuffer, fmt=*, iostat=errstat) inrow
+        if (errstat == IOSTAT_END) then
+            call stderr("Incomplete input row")
+            call EXIT(1)
+        end if
+
+        WRITE (unit=outunit, fmt=*) inrow
+    end do
+
+
+    ! Close input and output files if they were opened
+    if (inunit /= INPUT_UNIT) then
+        CLOSE (inunit)
+    end if
+    if (outunit /= OUTPUT_UNIT) then
+        CLOSE (outunit)
+    end if
+
+end program DO3SE
