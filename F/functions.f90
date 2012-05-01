ae3d1f800eacb75a57d1a01f64aa278ead369f60
Alan Briolat <alan.briolat@gmail.com>
2012-05-01 12:09:38 +0100

    Removed a load of functions that don't actually get used

--- F/functions.f90
+++ F/functions.f90
@@ -1,95 +1,16 @@
 module Functions
 
-    public :: Polygon
-    public :: newPolygon
     public :: deg2rad
-    public :: rad2deg
 
 contains
-    
-    !==========================================================================
-    !   
-    !  y|      ____________
-    !   |     /            \
-    !  z|    /              \___
-    !  x|___/                
-    !   |_______________________
-    !       a  b          c d
-    !
-    ! This function finds the value on the y-axis for a given x-axis 
-    ! value on a graph that follows the generic 3-level polygon 
-    ! shape
-    !==========================================================================
-    function Polygon(a, b, c, d, x, y, z, value) result(retval)
-        real, intent(in) :: a, b, c, d, x, y, z, value
-        real :: retval
 
-        if ( value < a ) then
-            retval = x
-        else if ( value < b ) then
-            retval = x + (((value - a)/(b - a)) * (y - x))
-        else if ( value < c ) then
-            retval = y
-        else if ( value < d ) then
-            retval = y - (((value - c)/(d - c)) * (y - z))
-        else
-            retval = z
-        end if
-    end function Polygon
+    pure function deg2rad(x) result (retval)
+        real, intent(in)    :: x        ! Angle in degrees
+        real                :: retval   ! Output: angle in radians
 
-    !==========================================================================
-    !   
-    !   |  x ____________
-    !   |   /            \
-    !   |w /              \
-    !   |  |               \ y
-    !  v|__|               |
-    !   |                  |___ z
-    !   |______________________
-    !      a b          c  d
-    !
-    ! TODO: Make this work
-    !
-    ! This function finds the value on the y-axis for a given x-axis 
-    ! value on a graph that follows a polygon shape
-    !==========================================================================
-    function newPolygon(a, b, c, d, v, w, x, y, z, value) result(retval)
-        real, intent(in) :: a, b, c, d, v, w, x, y, z, value
-        real :: retval
-
-        print *, v, w, x, y, z
-
-        if ( value < a ) then
-            retval = v
-        else if ( value < b ) then
-            retval = x + (((value - a)/(b - a)) * (x - w))
-        else if ( value < c ) then
-            retval = x
-        else if ( value < d ) then
-            retval = y + (((value - c)/(d - c)) * (x - z))
-        else
-            retval = z
-        end if
-    end function newPolygon
-
-    
-    !==========================================================================
-    ! Conversions between degrees and radians
-    !==========================================================================
-    pure function deg2rad(x) result(retval)
-        real, intent(in) :: x
-        real :: retval
         real, parameter :: D2R = 0.017453292519943295
 
         retval = x * D2R
     end function deg2rad
-
-    function rad2deg(x) result(retval)
-        use Constants, only: D2R => DEG2RAD
-        real, intent(in) :: x
-        real :: retval
-
-        retval = x / D2R
-    end function rad2deg
 
 end module Functions
