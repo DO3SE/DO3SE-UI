5e856f2e45d8f3e1035cad68909e3898d4d0306e
Alan Briolat <alan.briolat@gmail.com>
2012-05-28 17:01:45 +0100

    Create pure subroutine do3se_penman_monteith_hourly

--- do3se_soilwater.f90
+++ do3se_soilwater.f90
@@ -1,5 +1,6 @@
 module DO3SE_soilwater
 
+    public :: do3se_penman_monteith_hourly
     public :: do3se_Sn_diff
     public :: do3se_SMD
     public :: do3se_f_PAW
@@ -7,6 +8,93 @@ module DO3SE_soilwater
     private
 
 contains
+
+    ! =======================================================================
+    ! Calculate the evaporation of intercepted precipitation (Ei), potential
+    ! plant transpiration (PEt), actual plant transpiration (Et), actual
+    ! evapotranspiration (AEt) and soil evaporation (Es) using the
+    ! Penman-Monteith method.
+    ! =======================================================================
+    pure subroutine do3se_penman_monteith_hourly(Rn, VPD_Pa, P_Pa, Ts_C, LAI, &
+                                                 Rsto_c, Rsto_PEt, Ra, Rb_H2O, &
+                                                 Rinc, Rsoil, Es_blocked, &
+                                                 Ei_hr, PEt_hr, Et_hr, Es_hr, AEt_hr)
+        real, intent(in) :: Rn              ! Net radiation (J)
+        real, intent(in) :: VPD_Pa          ! Vapour pressure deficit (Pa)
+        real, intent(in) :: P_Pa            ! Atmospheric pressure (Pa)
+        real, intent(in) :: Ts_C            ! Air temperature (degrees C)
+        real, intent(in) :: LAI             ! Leaf area index (m2/m2)
+        real, intent(in) :: Rsto_c          ! Canopy stomatal resistance (s/m)
+        real, intent(in) :: Rsto_PEt        ! Canopy stomatal resistance without SMD influence (s/m)
+        real, intent(in) :: Ra              ! Aerodynamic resistance (s/m)
+        real, intent(in) :: Rb_H2O          ! Boundary layer resistance to water vapour (s/m)
+        real, intent(in) :: Rinc            ! In-canopy resistance (s/m)
+        real, intent(in) :: Rsoil           ! Soil resistance (s/m)
+        logical, intent(in) :: Es_blocked   ! Is soil evaporation blocked?
+        real, intent(out) :: Ei_hr          ! Output: hourly evaporation of intercepted precipitation (m/hour)
+        real, intent(out) :: PEt_hr         ! Output: hourly potential transpiration assuming non-limiting SMD (m/hour)
+        real, intent(out) :: Et_hr          ! Output: hourly plant transpiration (m/hour)
+        real, intent(out) :: Es_hr          ! Output: hourly evaporation from soil (m/hour)
+        real, intent(out) :: AEt_hr         ! Output: hourly evapotranspiration from plant and soil (m/hour)
+
+        real, parameter :: Ts_K = 273.16    ! Conversion from degrees Celsius to Kelvin
+        real, parameter :: Dratio = 0.662   ! Ratio between molecular diffusivity of O3 and H2O
+
+        real :: esat, eact, Tvir, delta, lambda, psychro, Pair, Cair, G
+
+        real :: Et_1, Et_2, Ei_3, PEt_3, Et_3
+        real :: t, Es_Rn, Es_G, Es_1, Es_2, Es_3
+        real :: SW_a, SW_s, SW_c, C_canopy, C_soil
+
+        esat = 611 * exp(17.27 * Ts_C / (Ts_C + 237.3))
+        eact = esat - VPD_Pa
+        Tvir = (Ts_c+Ts_K)/(1-(0.378*(eact/P_Pa)))
+        delta= ((4098*esat)/((Ts_c+237.3)**2))
+        lambda = (2501000-(2361*Ts_c))
+        psychro = 1628.6 * (P_Pa/lambda)
+        Pair = (0.003486*(P_Pa/Tvir))
+        Cair = (0.622*((lambda*psychro)/P_Pa))
+
+        G = 0.1 * Rn
+
+        Et_1 = (delta * (Rn - G)) / lambda
+        Et_2 = 3600 * Pair * Cair * VPD_Pa / Rb_H2O / lambda
+
+        Ei_3 = delta + psychro
+        Ei_hr = (Et_1 + Et_2) / Ei_3 / 1000
+
+        PEt_3 = delta + psychro * (1 + (Rsto_PEt * Dratio) / Rb_H2O)
+        PEt_hr = (Et_1 + Et_2) / PEt_3 / 1000
+
+        Et_3 = delta + psychro * (1 + (Rsto_c * Dratio) / Rb_H2O)
+        Et_hr = (Et_1 + Et_2) / Et_3 / 1000
+
+        if (Es_blocked) then
+            Es_hr = 0
+        else
+            t = exp(-0.5 * LAI)
+            Es_Rn = Rn * t
+            Es_G = 0.1 * Es_Rn
+            Es_1 = (delta * (Rn - G)) / lambda
+            Es_2 = ((3600 * Pair * Cair * VPD_Pa) - (delta * Rinc * ((Rn - G) - (Es_Rn - Es_G)))) / (Rinc + Rb_H2O) / lambda
+            Es_3 = delta + (psychro * (1.0 + (Rsoil / (Rb_H2O + Rinc))))
+            Es_hr = (Es_1 + Es_2) / Es_3 / 1000
+        end if
+
+        ! Calculate AEt from Et and Es (after Shuttleworth and Wallace, 1985)
+        SW_a = (delta + psychro) * (Ra + Rb_H2O)
+        SW_s = (delta + psychro) * Rinc + (psychro * Rsoil)
+        SW_c = (delta + psychro) * 0 + (psychro + Rsto_c) ! Boundary layer 
+                                                          ! resistance = 0
+        C_canopy = 1 / (1 + ((SW_c * SW_a) / (SW_s * (SW_c + SW_a))))
+        C_soil = 1 / (1 + ((SW_s * SW_a) / (SW_c * (SW_s + SW_a))))
+        if (Es_hr <= 0) then
+            AEt_hr = Et_hr
+        else
+            AEt_hr = (C_canopy * Et_hr) + (C_soil * Es_hr)
+        end if
+    end subroutine do3se_penman_monteith_hourly
+
 
     ! =======================================================================
     ! Calculate the change in volumetric water content due to precipitation
