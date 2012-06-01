ccc1a5b241f22224a28e4a93043b7ef080d19a67
Alan Briolat <alan.briolat@gmail.com>
2012-06-01 16:01:43 +0100

    Implement multiplicative Gsto as function
    Also introduces the GstoType type that contains the different stomatal
    conductances, for convenience.  Future functions should expect one of
    these instead of individual stomatal conductances whenever more than
    one is required.

--- do3se_resistance.f90
+++ do3se_resistance.f90
@@ -1,7 +1,24 @@
 module DO3SE_resistance
 
+    public :: GstoType
     public :: do3se_Ra_simple
     public :: do3se_Rb
+    public :: do3se_multiplicative_gsto
+
+    ! =========================================================================
+    ! Stomatal conductance (Gsto)
+    !
+    ! This type encapsulates the different variations of stomatal conductance.
+    ! All are in units of mmol O3 m-2 PLA s-1.
+    !
+    ! leaf:             mean Gsto of upper canopy leaf
+    ! mean:             mean Gsto of whole canopy
+    ! canopy:           total canopy Gsto, taking leaf area into account
+    ! canopy_noSMD:     same as "canopy", but assuming no SWP influence
+    ! =========================================================================
+    type GstoType
+        real :: leaf, mean, canopy, canopy_noSMD
+    end type GstoType
 
     private
 
@@ -39,5 +56,34 @@ contains
 
         rb = (2.0 / (K * ustar)) * (((V/d)/PR)**(2.0/3.0))
     end function do3se_Rb
+
+
+    ! =========================================================================
+    ! Calculate variations of stomatal conductance using the mulitplicative
+    ! model.
+    ! =========================================================================
+    pure elemental function do3se_multiplicative_gsto(gmax, fmin, gmorph, LAI, &
+            fphen, leaf_fphen, flight, leaf_flight, ftemp, fVPD, fSWP, fO3) result (gsto)
+        real, intent(in)    :: gmax         ! Maximum gsto (mmol O3 m-2 PLA s-1)
+        real, intent(in)    :: fmin         ! Minimum gsto fraction (fraction)
+        real, intent(in)    :: gmorph       ! Sun/shade morphology factor (fraction)
+        real, intent(in)    :: LAI          ! Leaf area index, for bulk gsto (m^2/m^2)
+
+        real, intent(in)    :: fphen        ! Phenology-related effect on gsto (fraction)
+        real, intent(in)    :: leaf_fphen   ! Phenology-related effect on leaf gsto (fraction)
+        real, intent(in)    :: flight       ! Irradiance effect on gsto (fraction)
+        real, intent(in)    :: leaf_flight  ! Irradiance effect on leaf gsto (fraction)
+        real, intent(in)    :: ftemp        ! Temperature effect on gsto (fraction)
+        real, intent(in)    :: fVPD         ! VPD effect on gsto (fraction)
+        real, intent(in)    :: fSWP         ! Soil water effect on gsto (fraction)
+        real, intent(in)    :: fO3          ! O3 effect on gsto (fraction)
+
+        type(GstoType)      :: gsto         ! Output: stomatal conductances (mmol O3 m-2 PLA s-1)
+
+        gsto%leaf = gmax * min(leaf_fphen, fO3) * leaf_flight * max(fmin, ftemp * fVPD * fSWP)
+        gsto%mean = gmax * gmorph * fphen * flight * max(fmin, ftemp * fVPD * fSWP)
+        gsto%canopy = gsto%mean * LAI
+        gsto%canopy_noSMD = gmax * fphen * flight * ftemp * fVPD * LAI
+    end function do3se_multiplicative_gsto
 
 end module DO3SE_resistance
