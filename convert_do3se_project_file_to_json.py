04c1d371c45d3a5523cde881559e9101cbf200b6
Sam Bland <sbland.co.uk@gmail.com>
2021-02-04 10:55:19 +0000

    Add convert do3se file to json

---
+++ convert_do3se_project_file_to_json.py
@@ -0,0 +1,20 @@
+"""This cli entrypoint is for running distributed multiruns."""
+
+import sys
+import os
+from do3se.project import Project
+from pprint import pprint
+import json
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    if len(args) != 1:
+        raise ValueError(
+            'Must only include project file location as argument.')
+
+    project_file_loc = args[0]
+    output_file_location = project_file_loc.split('.')[0] + '.json'
+    project = Project(project_file_loc)
+    with open(output_file_location, 'w') as outfile:
+        json.dump(dict(project.data), outfile)
