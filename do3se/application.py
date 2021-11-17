1b99754f4f8b7c2a47872169636a374d7501094c
Sam Bland <sbland.co.uk@gmail.com>
2021-11-17 12:03:12 +0000

    setup bumpversion

--- do3se/application.py
+++ do3se/application.py
@@ -19,7 +19,7 @@ import optparse
 import os.path
 app_name = 'DO3SE'
 app_description = 'Deposition of Ozone and Stomatal Exchange'
-app_version = '3.4.0'
+app_version = '3.4.2'
 
 _log = logging.getLogger('do3se.application')
 
