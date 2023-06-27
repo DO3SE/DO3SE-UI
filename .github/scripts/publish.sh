896f80da96632d35c49552d899edc50de1a43c80
sam bland <sbland.co.uk@gmail.com>
2023-06-27 10:42:47 +0100

    Update publish script

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -2,4 +2,4 @@
 # get version number from file using cat
 APP_VERSION=$(cat src/version)
 TAG="v$APP_VERSION"
-gh release create $TAG DO3SE-UI.zip -R https://github.com/SEI-DO3SE/do3se_releases_private
+gh release create $TAG dist/do3se-$APP_VERSION-*.whl -R https://github.com/SEI-DO3SE/do3se_releases_private
